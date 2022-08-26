0d23f7b89de1fc19cd05980f82549678f8c70925
Jonathan Naraja <j2naraja@gmail.com>
2022-08-26 13:32:34 -0700

    SSR with Node

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ import React from 'react';
 import styles from './index.module.css';
 import Card from './Card';
 import data from './API/data.json';
+import Link from 'next/link';
 
 export default class Index extends React.Component {
-    static async getInitialProps () {
+    static async getInitialProps() {
         return { cards: data }
     }
 
-    render () {
+    render() {
         return (
             <div className={styles.app}>
                 <header className={styles.header}>
-                    <img src="/logo.png"
-                        className={styles.logo} alt="logo"
-                    />
+                    <Link href= "/page3">
+                        <img src="/logo.png"
+                            className={styles.logo} alt="logo"
+                        />
+                    </Link>
+
                 </header>
                 <div className={styles.grid}>
-                   {
-                       this.props.cards.map((card) => (
-                           <Card key={card.id} />
-                       ))
-                   }
+                    {
+                        this.props.cards.map((card) => (
+                            <Card key={card.id} />
+                        ))
+                    }
                 </div>
             </div>
         )
